e0ba05cc55ad48981e8c1bf0c8bbe9beaee54199
Julian Dumebi Duru <dduru@teamapt.com>
2020-01-06 16:20:49 +0100

    - Implemented Adding Lottery User Campaign - Fixed bug in ObjectUtil

--- src/main/java/com/omarze/controller/LotteryUserCampaignController.java
+++ src/main/java/com/omarze/controller/LotteryUserCampaignController.java
@@ -2,36 +2,44 @@ package com.omarze.controller;
 
 
 import com.omarze.Constants;
+import com.omarze.api.dto.LotteryUserCampaignDTO;
 import com.omarze.entities.LotteryUserCampaign;
 import com.omarze.exception.ServiceException;
 import com.omarze.services.usercampaign.LotteryUserCampaignService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 /**
  * created by julian
  */
 @RestController
-@RequestMapping(Constants.API_BASE + "/user_campaigns")
-public class LotteryUserCampaignController {
+@RequestMapping(LotteryUserCampaignController.PATH)
+public class LotteryUserCampaignController extends ApiBaseController {
+
+    public final static String PATH = Constants.API_BASE + "/user_campaign";
+
 
     private LotteryUserCampaignService userCampaignService;
 
 
-    public LotteryUserCampaignController(LotteryUserCampaignService userCampaignService) {
+    @Autowired
+    public void setUserCampaignService(LotteryUserCampaignService userCampaignService) {
         this.userCampaignService = userCampaignService;
     }
 
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public LotteryUserCampaign addUserCampaign(LotteryUserCampaign userCampaign) throws ServiceException {
-        return userCampaignService.addUserCampaign(userCampaign);
+    public LotteryUserCampaignDTO addUserCampaign(
+            @Valid @RequestBody LotteryUserCampaignDTO userCampaignDTO
+    ) throws ServiceException {
+        LotteryUserCampaign userCampaign = userCampaignService.addUserCampaign(userCampaignDTO);
+        return map(userCampaign, LotteryUserCampaignDTO.class);
     }
 
 
-
 }
+
---
+++ src/main/java/com/omarze/services/usercampaign/LotteryUserCampaignServiceImpl.java
@@ -0,0 +1,49 @@
+package com.omarze.services.usercampaign;
+
+
+import com.omarze.api.dto.LotteryUserCampaignDTO;
+import com.omarze.entities.LotteryUserCampaign;
+import com.omarze.exception.ServiceException;
+import com.omarze.persistence.CampaignRepository;
+import com.omarze.persistence.LotteryUserCampaignRepository;
+import com.omarze.persistence.LotteryUserRepository;
+import com.omarze.services.campaign.NumberGenerator;
+import com.omarze.services.usercampaign.handlers.Save;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+/**
+ * created by julian
+ */
+@Service
+@RequiredArgsConstructor
+public class LotteryUserCampaignServiceImpl implements LotteryUserCampaignService {
+
+
+    private final LotteryUserCampaignRepository userCampaignRepository;
+
+
+    private final CampaignRepository campaignRepository;
+
+
+    private final LotteryUserRepository lotteryUserRepository;
+
+
+    private final NumberGenerator numberGenerator;
+
+
+    @Override
+    public LotteryUserCampaign addUserCampaign(LotteryUserCampaignDTO userCampaignDTO) throws ServiceException {
+        return Save.builder()
+            .userCampaignDTO(userCampaignDTO)
+            .userCampaignRepository(userCampaignRepository)
+            .numberGenerator(numberGenerator)
+            .campaignRepository(campaignRepository)
+            .lotteryUserRepository(lotteryUserRepository)
+            .build()
+            .execute();
+    }
+
+
+}
+
