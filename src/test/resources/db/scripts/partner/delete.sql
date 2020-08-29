6d14a0086e12c2308cbc7690594023af56faabbf
Julian D Duru <durutheguru@gmail.com>
2020-08-29 20:37:24 +0100

    ...

--- src/test/resources/db/scripts/partner/delete.sql
+++ src/test/resources/db/scripts/partner/delete.sql
@@ -3,6 +3,8 @@ DELETE FROM lottery_user_campaign;
 
 DELETE FROM campaign_image;
 
+DELETE FROM campaign_stage_evaluation_result_winning_numbers;
+
 DELETE FROM campaign_stage_evaluation_result;
 
 DELETE FROM stage_description;
