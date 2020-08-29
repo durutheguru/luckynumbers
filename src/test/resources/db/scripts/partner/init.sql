b12d3d6d65fb3ccb658e64355ecdadbc8383da19
Julian D Duru <durutheguru@gmail.com>
2020-08-29 20:43:52 +0100

    ...

--- src/test/resources/db/scripts/partner/init.sql
+++ src/test/resources/db/scripts/partner/init.sql
@@ -1,18 +1,18 @@
 
 INSERT INTO partner(id, name, description, time_added)
-  VALUES(1, 'Burna Boy', 'African Giant', CURRENT_TIMESTAMP);
+  VALUES(10000, 'Burna Boy', 'African Giant', CURRENT_TIMESTAMP);
 
 
 INSERT INTO partner(id, name, description, time_added)
-  VALUES(2, 'Dangote', 'Still dey find money', CURRENT_TIMESTAMP);
+  VALUES(20000, 'Dangote', 'Still dey find money', CURRENT_TIMESTAMP);
 
 
 INSERT INTO partner_user(id, partner_id, name, email, username, password, time_added)
-  VALUES(1, 1, 'Burna''s Manager', 'burna_manager@yahoo.co.uk', 'burna_manager', 'password', CURRENT_TIMESTAMP);
+  VALUES(10000, 10000, 'Burna''s Manager', 'burna_manager@yahoo.co.uk', 'burna_manager', 'password', CURRENT_TIMESTAMP);
 
 
 INSERT INTO partner_user(id, partner_id, name, email, username, password, time_added)
-  VALUES(2, 2, 'Dangote''s Assistant', 'dangote@dangote.com', 'dangote_assistant', 'password', CURRENT_TIMESTAMP);
+  VALUES(20000, 20000, 'Dangote''s Assistant', 'dangote@dangote.com', 'dangote_assistant', 'password', CURRENT_TIMESTAMP);
 
 
 
